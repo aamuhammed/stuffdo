79e70b8592c3b12d3d5ed5bf79593d6268df0658
Abdullahi <shinamuh@yahoo.com>
2016-10-18 04:17:48 +0100

    Added states to tasks

--- config/application.rb
+++ config/application.rb
@@ -4,10 +4,13 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 module Stuffdo
   class Application < Rails::Application
+     # the new line added for autoload of lib
+    config.autoload_paths += %W(#{config.root}/lib)
+   
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -21,6 +24,7 @@ module Stuffdo
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
   end
 end
+
