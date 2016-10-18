10aa65d01ee82569866b0b68505cf87ca814b07c
Abdullahi <shinamuh@yahoo.com>
2016-10-18 02:59:44 +0100

    Created User-Task association, updated controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
+ belongs_to :user
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :tasks, dependent: :destroy 
 end
